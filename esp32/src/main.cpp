0faccdeb02006f031fdfebab3b92d3ed849db6ca
kokodoodle <simonewijnen290@hotmail.com>
2021-04-12 15:13:49 +0200

    esp32 + wifi

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,12 +1,53 @@
 #include <Arduino.h>
+#include <WiFi.h>
+
+const char *ssid = "hoi_simone";
+const char *password = "hallo123";
+
+void scan_networks() { // function to scan local networks in area
+
+  WiFi.disconnect(); // disconnect if previously disconnected
+
+  uint8_t number_of_networks = WiFi.scanNetworks(); // scan different networks
+
+  Serial.println(number_of_networks); 
+
+  for (int i = 0; i < number_of_networks; i++) { // loop to present network name + signal strength + mac address
+    Serial.print("Network name: ");
+    Serial.println(WiFi.SSID(i));
+
+    Serial.print("Signal strength: ");
+    Serial.println(WiFi.RSSI(i));
+    
+    Serial.print("MAC address: ");
+    Serial.println(WiFi.BSSIDstr(i));
+
+    Serial.println("-----------------------");
+  }
+}
+
+void connect_to_network() { // function to connect to current WiFi network
+
+  WiFi.mode(WIFI_STA); // mode = station mode
+  WiFi.begin(ssid, password); // initialize WiFi using networkname + password
+
+  Serial.println();
+
+  while (WiFi.status() != WL_CONNECTED) { // wait while connected has not been made yet
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP()); // show IP address if connection has been made
+}
 
 void setup() {
-  Serial.begin(115200);
+
+  Serial.begin(115200); 
+
+  scan_networks();
+  connect_to_network();
 }
 
-void loop() {
-  Serial.println("LED is on");
-  delay(1000);
-  Serial.println("LED is off");
-  delay(1000);
-}
+void loop() { 
+
+} 
