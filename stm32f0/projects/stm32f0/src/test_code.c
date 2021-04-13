3e706b711051e3d311cf961bcc5542b88d7d6c37
Tiemen Molenaar <tiemen131415@gmail.com>
2021-04-13 11:30:33 +0200

    test code

--- stm32f0/projects/stm32f0/src/test_code.c
+++ stm32f0/projects/stm32f0/src/test_code.c
@@ -1,7 +1,7 @@
 // is a test dummy temprature sensor 
 float test_read_tempture(void) {
  static int i=0;
- const float values[] = {24.0, 24.0, 23.0, 23.5,22.0,20.0,20.0,20.0,20.0};
-  if(i >= 9) i = 0; 
+ const float values[] = {1,2,3,4,5,6};
+  if(i >= 6) i = 0; 
  return values[i++];
 }
