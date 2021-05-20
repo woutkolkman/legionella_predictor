fd70f773bd838ab4b9f471ed51bf0ba6784f196e
stfnkts <stefan221100@gmail.com>
2021-05-20 12:26:25 +0200

    files naar juiste plek

--- stm32f0/projects/stm32f0/inc/struct.h
+++ stm32f0/projects/stm32f0/inc/struct.h
@@ -4,12 +4,11 @@
 #include "stm32f0xx.h"
 #include "stdbool.h"
 
-#define TEMPERATURE_SIZE    60
+#define TEMPERATURE_SIZE 60
 #define TRANSMITTER_ID_SIZE 8
 
 struct DATA {
 	uint8_t transmitter_ID[TRANSMITTER_ID_SIZE];
-	unsigned long hour;
 	uint8_t Temperature[TEMPERATURE_SIZE];
 };
 
